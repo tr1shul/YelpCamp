da23820fb35a4defdcc6d41fc27a31f3fca7336c
Purv Chauhan <purvchauhan@gmail.com>
2018-12-12 11:47:09 -0700

    Added Data Persistence! MongoDB and Mongoose finally come together to save all the campgrounds.

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
+mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-  {name: "Salmon Creek", image: "https://source.unsplash.com/random"},
-  {name: "Granite Hill", image: "https://source.unsplash.com/random"},
-  {name: "Mountain Goat's Rest", image: "https://source.unsplash.com/random"}
-];
+//SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema(
+  {
+    name: String,
+    image: String
+  }
+);
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//   {
+//     name: "Granite Hill",
+//     image: "https://source.unsplash.com/random"
+//   },
+//   function(err, campground)
+//   {
+//     if(err)
+//     {
+//       console.log(err);
+//     }
+//     else
+//     {
+//       console.log("Newly Created Campground");
+//       console.log(campground);
+//     }
+//   });
 
 app.get("/", function(req, res)
 {
@@ -18,7 +42,17 @@ app.get("/", function(req, res)
 
 app.get("/campgrounds", function(req, res)
 {
-  res.render("campgrounds", {campgrounds: campgrounds});
+  Campground.find({}, function(err, allCampgrounds)
+  {
+    if(err)
+    {
+      console.log(err);
+    }
+    else
+    {
+      res.render("campgrounds", {campgrounds: allCampgrounds});
+    }
+  })
 });
 
 app.post("/campgrounds", function(req, res)
@@ -26,7 +60,17 @@ app.post("/campgrounds", function(req, res)
   var name = req.body.name;
   var image = req.body.image;
   var newCampround = {name: name, image: image};
-  campgrounds.push(newCampround);
+  Campground.create(newCampround, function(err, newlyCreated)
+  {
+    if(err)
+    {
+      console.log(err);
+    }
+    else
+    {
+      res.redirect("/campgrounds");
+    }
+  });
   res.redirect("/campgrounds");
 });
 
