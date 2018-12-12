469c14fd234d2e564fdec1cb706fa09ee92f5607
Purv Chauhan <purvchauhan@gmail.com>
2018-12-12 00:06:55 -0700

    Added a navbar to header

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var campgrounds = [
-  {name: "Salmon Creek", image: "https://picsum.photos/200/300/?random"},
-  {name: "Granite Hill", image: "https://picsum.photos/200/300/?random"},
-  {name: "Mountain Goat's Rest", image: "https://picsum.photos/200/300/?random"}
+  {name: "Salmon Creek", image: "https://source.unsplash.com/random"},
+  {name: "Granite Hill", image: "https://source.unsplash.com/random"},
+  {name: "Mountain Goat's Rest", image: "https://source.unsplash.com/random"}
 ];
 
 app.get("/", function(req, res)
