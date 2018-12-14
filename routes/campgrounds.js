260da395076f0a07284a072f7bb412c9cd2f49a7
Purv Chauhan <purvchauhan@gmail.com>
2018-12-13 22:55:43 -0700

    Added a Edit and Update Route, installed the method-override library.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -68,6 +68,39 @@ router.get("/:id", function(req, res)
   })
 });
 
+//EDIT -
+router.get("/:id/edit", function(req, res)
+{
+  Campground.findById(req.params.id, function(err, foundCampground)
+  {
+    if(err)
+    {
+      res.redirect("/campgrounds");
+    }
+    else
+    {
+      res.render("campgrounds/edit", {campground: foundCampground});
+    }
+  });
+});
+
+//UPDATE -
+router.put("/:id", function(req, res)
+{
+  Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updateCampground)
+  {
+    if(err)
+    {
+      res.redirect("/campgrounds");
+    }
+    else
+    {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
+
+//DESTROY -
 //middleware
 function isLoggedIn(req, res, next)
 {
